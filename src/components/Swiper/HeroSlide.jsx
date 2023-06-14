2995a98908e0afc800f744771b5ef1cbc4f9742e
RoanMacmillan <hibee94@gmail.com>
2023-06-14 16:45:12 +0100

    image fix

--- src/components/Swiper/HeroSlide.jsx
+++ src/components/Swiper/HeroSlide.jsx
@@ -7,11 +7,11 @@ import useIntersectionObserver from "../../components/useIntersectionObserver/us
 import { Link } from "react-router-dom";
 import arrow from "../../assets/images/icon-arrow.svg";
 import img1 from "../../assets/images/1.jpg";
-import img2 from "../../assets/images/2.jpg";
+import img2 from "../../assets/images/2.webp";
 import img3 from "../../assets/images/3.jpg";
 import img4 from "../../assets/images/4.jpg";
 
-import img1mobile from "../../assets/images/1mobile.jpg";
+import img1mobile from "../../assets/images/1mobile.webp";
 import img2mobile from "../../assets/images/2mobile.jpg";
 import img3mobile from "../../assets/images/3mobile.jpg";
 import img4mobile from "../../assets/images/4mobile.jpg";
