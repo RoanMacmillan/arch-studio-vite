a236154815ed58b86f4bca99d41cdda3c08b2dc1
RoanMacmillan <hibee94@gmail.com>
2023-06-14 17:29:48 +0100

    marker fix

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,23 @@
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import styles from './Map.module.css';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 function MapComponent({ targetLocation, zoomLevel }) {
+
+  let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconSize: [25, 41], // size of the icon
+    iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+    popupAnchor: [1, -34], // point from which the popup should open relative to the iconAnchor
+    shadowSize: [41, 41] // size of the shadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
   const defaultCenter = [37.7749, -122.4194]; // Default position coordinates (San Francisco)
   const center = targetLocation || defaultCenter; // Use targetLocation if provided, otherwise use the default
 
